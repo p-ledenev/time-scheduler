0f180dc9107938a73a5c4d453621936a4b09e507
ledenev pavel <pledenev@gmail.com>
2019-05-16 23:04:10 +0300

    Initial commit

---
+++ src/main/java/task/scheduler/Application.java
@@ -0,0 +1,8 @@
+package task.scheduler;
+
+public class Application {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/test/java/task/scheduler/testing/ApplicationTesting.java
@@ -0,0 +1,11 @@
+package task.scheduler.testing;
+
+import org.junit.Test;
+
+public class ApplicationTesting {
+
+    @Test
+    public void shouldDoSmth() {
+        System.out.println("Do smth");
+    }
+}
